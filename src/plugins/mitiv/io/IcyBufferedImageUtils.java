e2acf0eeba354afcdf911746a7c95390738d3c34
Lightjohn <johnleger26@gmail.com>
2015-03-20 14:59:01 +0100

    Adding deprecated on old method, just to be sure

--- src/plugins/mitiv/io/IcyBufferedImageUtils.java
+++ src/plugins/mitiv/io/IcyBufferedImageUtils.java
@@ -215,6 +215,7 @@ public class IcyBufferedImageUtils {
      */
 
     //FIXME TMP COPY
+    @Deprecated
     public static double[] icyImage3DToArray1D(ArrayList<IcyBufferedImage>listImage, int width, int height, int sizeZ, boolean isComplex) {
         double[] out;
         if (isComplex) {
