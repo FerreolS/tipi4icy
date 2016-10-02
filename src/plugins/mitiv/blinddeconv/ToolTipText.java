89a9d44912e9b4e2262f2a5a28d5a794a2218ab2
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2016-10-02 10:34:46 +0200

    Update tool-tips

--- src/plugins/mitiv/blinddeconv/ToolTipText.java
+++ src/plugins/mitiv/blinddeconv/ToolTipText.java
@@ -2,7 +2,7 @@
  * This file is part of TiPi (a Toolkit for Inverse Problems and Imaging)
  * developed by the MitiV project.
  *
- * Copyright (c) 2014 the MiTiV project, http://mitiv.univ-lyon1.fr/
+ * Copyright (c) 2014-2016 the MiTiV project, http://mitiv.univ-lyon1.fr/
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -28,31 +28,31 @@ package plugins.mitiv.blinddeconv;
 /**
  * In this class is contained all the tooltip texts. It can be usefull for correcting mistakes or
  * new languages.
- * 
+ *
  * @author light
  *
  */
 public class ToolTipText {
 
-	public static final String deconvolutionSlider = "Update Mu value";
-	public static final String sequenceImage = "Stack to be deconvolved";
+    public static final String deconvolutionSlider = "Update Mu value";
+    public static final String sequenceImage = "Stack to be deconvolved";
     public static final String sequencePSF = "The PSF associated to the image given";
     public static final String sequenceWeigth = "Map of space varying noise variance or precision";
     public static final String sequenceVariance = "The variance map";
     public static final String sequencePixel = "The binary map with 0 for bad pixels";
-    
+
     public static final String doubleGrtoll = "Relative gradient tolerance for the convergence";
     public static final String doubleMu = "Hyper-parameter";
     public static final String doubleEpsilon = "TV threshold Epsilon";
     public static final String doubleMaxIter = "Maximum number of iterations";
     public static final String doublePadding = "Pad with X lines";
-    public static final String doubleGain = "The gain in e-/level";
-    public static final String doubleNoise = "The readout noise i.e the RMS in e-/pixel";
+    public static final String doubleGain = "The detector gain in electrons per analog digital unit (ADU)";
+    public static final String doubleNoise = "The standard deviation of the readout noise in e-/pixel";
     public static final String doubleBDecTotalIteration = "The maximum number of loops of the algorithm, the higher the potentially longer";
     public static final String doubleGrtolPhase = "Relative gradient tolerance for the convergence of the phase coeficients";
     public static final String doubleGrtolModulus = "Relative gradient tolerance for the convergence of the modulus coeficients";
     public static final String doubleGrtolDefocus = "Relative gradient tolerance for the convergence of the defocus coeficients";
-    
+
     public static final String doubleDxy = "Lateral pixel size (nm)";
     public static final String doubleDz = "Axial pixel size (nm)";
     public static final String doubleNxy = "Number of pixels along XY-axis";
@@ -64,13 +64,13 @@ public class ToolTipText {
     public static final String doubleNbeta = "Number of zernike describing the pupil modulus";
     //static final String booleanPSFSplitted = "If the PSF is not centered check this box";
     public static final String booleanPSFSplitted = "<html><pre>"
-            + "Is the PSF:<br/>"
-            + " ---------      ---------     <br/>"
-            + " |       |      |*     *|     <br/>"
-            + " |   *   |  OR  |       |     <br/>"
-            + " |       |      |*     *|     <br/>"
-            + " ---------      ---------     <br/>"
-            + "</pre></html>";
+        + "Is the PSF:<br/>"
+        + " ---------      ---------     <br/>"
+        + " |       |      |       |     <br/>"
+        + " |   *   |  or  |       |     <br/>"
+        + " |       |      |*      |     <br/>"
+        + " ---------      ---------     <br/>"
+        + "</pre></html>";
     public static final String booleanRestart = "Restart from previous result, if enabled will start with last image and PSF";
     public  static final String booleanPositivity = "Enforce the positivity of the solution";
     public static final String booleanCrop = "Crop the output with the same field of view as the input.";
@@ -78,16 +78,3 @@ public class ToolTipText {
     public static final String textCanal = "Choose the image canal to use for the deconvolution";
     public static final String textOutput = "The output size as the nearest power of 2 size";
 }
-
-
-/*
- * Local Variables:
- * mode: Java
- * tab-width: 8
- * indent-tabs-mode: nil
- * c-basic-offset: 4
- * fill-column: 78
- * coding: utf-8
- * ispell-local-dictionary: "american"
- * End:
- */
