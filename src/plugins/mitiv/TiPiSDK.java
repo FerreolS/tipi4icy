0ff1a121f70e1adf72f046bfcd18a316c8724a9f
ferreol.soulez@free.fr <ferreol.soulez@free.fr>
2016-10-09 20:10:34 +0200

    add a class to build the TiPiSDK plugin

---
+++ src/plugins/mitiv/TiPiSDK.java
@@ -0,0 +1,15 @@
+package plugins.mitiv;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+public class TiPiSDK extends Plugin implements PluginLibrary {
+    public static void main(String [] args) {
+        System.out.println("TiPiSDK\n"
+                + "See https :// github .com/emmt/TiPi for the  source   code\n"
+                + "\n"
+                + "You can contact us at:\n"
+                + "https://github.com/emmt/TiPi/issues\n"
+                + "\n");
+    }
+}
